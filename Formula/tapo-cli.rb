3fa26d542a867754f5b3df8bffa23c98d8c4d88f
macswag <macswag@users.noreply.github.com>
2026-04-08 23:37:42 -0400

    Bump tapo-cli to v1.1.0

--- Formula/tapo-cli.rb
+++ Formula/tapo-cli.rb
@@ -3,8 +3,8 @@ class TapoCli < Formula
 
   desc "Control Tapo cameras from the terminal"
   homepage "https://github.com/mac1010z/tapo-cli"
-  url "https://github.com/mac1010z/tapo-cli/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "a2f4e1e9025c22a6c1503c32d75aafb7e2e7fd8e50f2f918cc2c8a20ba767bfb"
+  url "https://github.com/mac1010z/tapo-cli/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "8c5e43b152825410470d4c9ee7e523b582f7b940acbf35e28fc767685a00cef5"
   license "MIT"
 
   depends_on "python@3.13"
